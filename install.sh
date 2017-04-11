b9f9e1071dc6c3ef468e2546d2348c1f2aa94209
Proud Indian <rrangarajan.85@gmail.com>
2017-04-11 12:56:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 # tool to split the pdf in command line
 sudo apt-get install pdftk
+# command to use above tools
+# pdftk full-pdf.pdf cat 12-15 output outfile_p12-15.pdf
+# pdftk A=in.pdf cat A1-10 A15 A17 output out.pdf
