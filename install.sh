ed84689b6c1975099bf49fc6e5b7e8b7619d4f53
Proud Indian <rrangarajan.85@gmail.com>
2017-04-22 14:17:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,17 @@ sudo apt-get install pdftk
 # command to use above tools
 # pdftk full-pdf.pdf cat 12-15 output outfile_p12-15.pdf
 # pdftk A=in.pdf cat A1-10 A15 A17 output out.pdf
+sudo apt-get install tcpdump
+sudo apt-get install pysdm
+sudo apt-get install clisp
+sudo apt-get install elisp
+sudo apt-get install lisp
+sudo apt-get install unity-tweak-tool
+sudo apt-get install numix-icon-theme-circle
+sudo apt-get install numix-gtk-theme numix-icon-theme-circle
+sudo apt-get install numix-wallpaper-*
+sudo apt-get install apache24
+sudo apt-get install emacs
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install gdb
